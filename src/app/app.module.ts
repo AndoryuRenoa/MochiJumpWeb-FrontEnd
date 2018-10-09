73ea02e40a831e6e0d770279c5aa769c2b526d1c
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-10-09 16:13:58 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,24 @@ import { AboutComponent } from './about/about.component';
 import { MochiFaceComponent } from './mochi-face/mochi-face.component';
 import { LevelEditButtonComponent } from './main-menu/level-edit-button/level-edit-button.component';
 import { AboutButtonComponent } from './main-menu/about-button/about-button.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  {path: 'mainmenu', component: MainMenuComponent},
+  { path: '',   redirectTo: '/mainmenu', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+    
+
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+ ],
   declarations: [ AppComponent, HelloComponent, LoginComponent, MainMenuComponent, DrawLevelComponent, CanvasComponent, DrawLevelMenuComponent, AboutComponent, MochiFaceComponent, LevelEditButtonComponent, AboutButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/main-menu/level-edit-button/level-edit-button.component.ts
+++ src/app/main-menu/level-edit-button/level-edit-button.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-level-edit-button',
+  
   templateUrl: './level-edit-button.component.html',
+  
   styleUrls: ['./level-edit-button.component.css']
 })
 export class LevelEditButtonComponent implements OnInit {
+
+  // need to have test for whether or not browser is desktop 
+  // if not desktop html /css needs to be changed
 
   constructor() { }
 
